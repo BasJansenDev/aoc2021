873d168b00809c69439f1d561529f15739868577
Bas Jansen <basj9559@gmail.com>
2021-12-02 06:28:50 +0100

    Day 2

---
+++ day2/part1.py
@@ -0,0 +1,23 @@
+import operator
+
+
+def main():
+    lst = inputAsList()
+    res = (0,0)
+    for i in range (0,len(lst)):
+        entry = lst[i].split(' ')
+        direction = DIRECTIONS[entry[0]]
+        magnitude = int(entry[1])
+        res = tuple(map(operator.add,res, (direction[0] * magnitude,direction[1] * magnitude)))
+    return res[0]*res[1]
+
+DIRECTIONS = {
+    'forward': (1, 0),
+    'down': (0,1),
+    'up': (0, -1)
+}
+def inputAsList():
+    f = open('input')
+    return list(f.read().splitlines())
+
+print(main())
