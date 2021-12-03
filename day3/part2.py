1613bcae5ed0078800b1115c68d6512cda0f742b
Bas Jansen <basj9559@gmail.com>
2021-12-03 08:31:52 +0100

    Reduced readability of Part 2 by 10%

--- day3/part2.py
+++ day3/part2.py
@@ -4,28 +4,23 @@ def main():
     b = calc(lst, True)
     return int(a, 2) * int(b, 2)
 
-
-def calc(lst, inv):
-    res = lst
+def calc(res, inv):
     idx = 0
     while len(res) > 1:
         ones = 0
         for byte in res:
             if int(byte[idx]):
                 ones += 1
-        mcb = ones >= len(res) / 2
         resTmp = []
         for byte in res:
-            if int(byte[idx]) == (mcb ^ inv):
+            if int(byte[idx]) == ((ones >= len(res) / 2) ^ inv):
                 resTmp += [byte]
         res = resTmp
         idx += 1
     return res[0]
 
-
 def inputAsList():
     f = open('input')
     return list(f.read().splitlines())
-
 
 print(main())
