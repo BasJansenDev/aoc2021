a9557e8909dcab922faa3357dce50061bec12c0d
Bas Jansen <basj9559@gmail.com>
2021-12-03 09:45:31 +0100

    Cleaned up both solutions, included commentary to improve readability again

--- day3/part1.py
+++ day3/part1.py
@@ -3,22 +3,16 @@ from functools import reduce
 
 def main():
     lst = inputAsList()
-    ones = [0] * len(lst[0])
-    for byte in lst:
-        for bit in range(len(byte)):
-            ones[bit] += int(byte[bit])
-    bn = reduce(lambda a, b: (a<<1) + int(b), list(map(lambda x : x > len(lst)/2,ones)))
-    return bn * (2**len(lst[0])-1-bn)
 
-def small():
-    lst = inputAsList()
-    bn = reduce(lambda a, b: (a << 1) + int(b), list(map(lambda x: x > len(lst) / 2, list(map(lambda bit : sum([int(byte[bit]) for byte in lst]), range(len(lst[0])))))))
-    return bn * (2 ** len(lst[0]) - 1 - bn)
-
+    # Sum each bit in the list of bits by index:
+    ones = list(map(lambda bit : sum([int(bits[bit]) for bits in lst]), range(len(lst[0]))))
+    # Binary to decimal:
+    dec = reduce(lambda a, b: (a<<1) + int(b), list(map(lambda x : x > len(lst)/2, ones)))
+    # Multiply value with its inverse:
+    return dec * (2**len(lst[0])-1-dec)
 
 def inputAsList():
     f = open('input')
     return list(f.read().splitlines())
 
 print(main())
-print(small())
--- day3/part2.py
+++ day3/part2.py
@@ -6,8 +6,14 @@ def main():
 
 def calc(res, inv):
     idx = 0
+
+    # For bits[idx], take the sum to determine if 0 or 1 is the most common for that bit.
+    # Then, filter the original list based on whether a set of bits has the most common bit in idx.
+    # The inv parameter inverses the results, taking the least common bit for an index instead.
+    # Repeat until there is only one entry left.
+
     while len(res) > 1:
-        res = list(filter(lambda byte : int(byte[idx]) == ((sum(int(byte[idx]) for byte in res) >= len(res) / 2) ^ inv),res))
+        res = list(filter(lambda bits : int(bits[idx]) == ((sum(int(bits[idx]) for bits in res) >= len(res) / 2) ^ inv),res))
         idx += 1
     return res[0]
 
