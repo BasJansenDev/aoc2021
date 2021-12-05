47b2c7a6f9088659a95a7e1434a16aea1d688c5f
Bas Jansen <basj9559@gmail.com>
2021-12-05 19:57:30 +0100

    Made day 4 a bit smaller

--- day4/day4.py
+++ day4/day4.py
@@ -1,31 +1,19 @@
-def main1(inp):
+def main(inp,part1):
     numbers = inp[0].split(',')
     boards = make_board_matrix(inp[1:])
     for number in numbers:
         boardTmp = []
         for board in boards:
             updatedBoard = list(map(lambda row : [0 if number == value else value for value in row], board))
-            boardTmp.append(updatedBoard)
-            if(board_check(updatedBoard)):
-                return int(number) * calculate_leftover(updatedBoard)
-        boards = boardTmp
-    return 0
-
-def main2(inp):
-    numbers = inp[0].split(',')
-    boards = make_board_matrix(inp[1:])
-    for number in numbers:
-        boardTmp = []
-        for board in boards:
-            updatedBoard = list(map(lambda row : [0 if number == value else value for value in row], board))
-            win = board_check(updatedBoard)
-            if not win:
+            if not board_check(updatedBoard):
                 boardTmp.append(updatedBoard)
-            elif len(boards) == 1:
-                return int(number) * calculate_leftover(updatedBoard)
+            else:
+                if part1:
+                    return int(number) * calculate_total(updatedBoard)
+                elif len(boards) == 1:
+                    return int(number) * calculate_total(updatedBoard)
         boards = boardTmp
     return 0
-
 
 def board_check(board):
     transposedBoard = list(map(list,(zip(*board))))
@@ -34,12 +22,8 @@ def board_check(board):
     return a or b
 
 
-def calculate_leftover(board):
-    totalSum = 0
-    for row in board:
-        totalSum += sum(int(i) for i in row)
-    return totalSum
-
+def calculate_total(board):
+    return sum([sum(int(i) for i in row) for row in board])
 
 def input_as_list(inp):
     f = open(inp)
@@ -51,5 +35,5 @@ def make_board_matrix(inp):
         boards.append([i.replace('  ',' ').strip(' ').split(' ') for i in board])
     return boards
 
-print('Part 1: ' + str(main1(input_as_list('input'))))
-print('Part 2: ' + str(main2(input_as_list('input'))))
+print('Part 1: ' + str(main(input_as_list('input'),True)))
+print('Part 2: ' + str(main(input_as_list('input'),False)))
