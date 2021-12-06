a429aacee21b287040ec4edb09de871d2cce0fbb
Bas Jansen <basj9559@gmail.com>
2021-12-06 12:33:56 +0100

    Minimalized

--- day6/day6.py
+++ day6/day6.py
@@ -3,18 +3,12 @@ from functools import lru_cache
 
 def main(inp, days):
     fishlist = [int(j) for j in [i.split(',') for i in inp][0]]
-    total = 0
-    for fish in fishlist:
-        total += recurse(fish, days)
-    return total
+    return sum(recurse(fish,days) for fish in fishlist)
 
 
 @lru_cache(maxsize=None)
 def recurse(value, days):
-    if days <= value:
-        return 1
-    else:
-        return recurse(8, days - value - 1) + recurse(6, days - value - 1)
+    return 1 if days <= value else recurse(8, days - value - 1) + recurse(6, days - value - 1)
 
 
 def input_as_list(inp):
