7cd3f19db59ed32612f673794d041b5a7cf1a973
Bas Jansen <basj9559@gmail.com>
2021-12-06 12:27:39 +0100

    TIL you can remove the lru_cache limit

--- day6/day6.py
+++ day6/day6.py
@@ -1,17 +1,20 @@
-def main(inp, part2):
+from functools import lru_cache
+
+
+def main(inp, days):
     fishlist = [int(j) for j in [i.split(',') for i in inp][0]]
-    days = 20
-    for day in range (0,days):
-        fishListTmp = []
-        for fish in fishlist:
-            if(fish == 0):
-                fish = 7
-                fishListTmp.append(8)
-            fish -= 1
-            fishListTmp.append(fish)
-        fishlist = fishListTmp
-        print(fishlist)
-    return len(fishlist)
+    total = 0
+    for fish in fishlist:
+        total += recurse(fish, days)
+    return total
+
+
+@lru_cache(maxsize=None)
+def recurse(value, days):
+    if days <= value:
+        return 1
+    else:
+        return recurse(8, days - value - 1) + recurse(6, days - value - 1)
 
 
 def input_as_list(inp):
@@ -19,5 +22,5 @@ def input_as_list(inp):
     return list(f.read().splitlines())
 
 
-print('Part 1: ' + str(main(input_as_list('testinput'), False)))
-# print('Part 2: ' + str(main(input_as_list('testinput'), True)))
+print('Part 1: ' + str(main(input_as_list('input'), 80)))
+print('Part 2: ' + str(main(input_as_list('input'), 256)))
